b01c715e719d67419166f325ef5853afa306cc87
Jesus Ramirez <ramirez_eco@hotmail.com>
2017-11-23 16:02:51 -0600

    modified current optimizer and added 2 optimizers

--- cvar_optimization.py
+++ cvar_optimization.py
@@ -96,18 +96,22 @@ def cornish_fisher_expansion(q, k1, k2, k3, k4, k5):
     x_q += (z**4 - 6*z**2 + 3)/120*k5 - (z**4 - 5*z**2 + 2)/24*k3*k4 + (12*z**4 - 53*z**2 + 17)/324*k3**3
     return x_q
 
-
-# objective function
-def minimize_cvar(new_data, quantile, initial, method='SLSQP', display=False):
+# minimum-VaR portfolio given a minimum portfolio return (Markowitz optimization)
+def mean_mvar_optimization(data, min_return, initial, quantile=0.05, expected_returns=None, display=False):
     """
     Compute optimal weights after minimizing the cvar for given a quantile 
     level
     
     Parameters
     ----------
-        new_data: array_like of shape(M, N) with N > 1
-        quantile: scalar between 0 and 1
+        data: array_like of shape(M, N) with N > 1
+        min_return: scalar float
+            specifies the minimum return that the portfolio can attain
         initial: array_like of shape(N, )
+        quantile: scalar between 0 and 1, default value 0.05
+        expected_returns: array_like of shape(N, )
+        display: boolean, default value is False
+            if it is True, a summary of the optimization procedure is shown
     
     Return
     ------
@@ -115,11 +119,14 @@ def minimize_cvar(new_data, quantile, initial, method='SLSQP', display=False):
             among other features after optimization
     
     """
+    if expected_returns == None:
+        expected_returns = np.mean(data, axis=0)
+
     def func(weights):
         """
         Objective function value to optimize
         """
-        portfolio_returns = new_data@weights
+        portfolio_returns = data@weights
         return - approx_quantile(portfolio_returns, quantile)
     
     # constraints
@@ -140,6 +147,12 @@ def minimize_cvar(new_data, quantile, initial, method='SLSQP', display=False):
         Contraint 3: Weights are less than 1
         """
         return 1 - weights
+
+    def const_4(weights):
+        """
+        Contraint 4: portfolio return can be lower than a given fixed minimum return
+        """
+        return weights@expected_returns - min_return
     
     # setting up constraints as dictionary
     cons = ({'type': 'eq',
@@ -147,11 +160,149 @@ def minimize_cvar(new_data, quantile, initial, method='SLSQP', display=False):
            {'type': 'ineq',
            'fun': lambda x: const_2(x)},
            {'type': 'ineq',
-           'fun': lambda x: const_3(x)},)
+           'fun': lambda x: const_3(x)},
+           {'type': 'ineq',
+           'fun': lambda x: const_4(x)})
     
     if method=='SLSQP':
         # Sequential Least SQuares Programming (SLSQP)
-        return minimize(func, initial, constraints=cons, method='SLSQP', options={'disp': True})
+        res = minimize(func, initial, constraints=cons, method='SLSQP', options={'disp': display})
+        return res
     else:
         print('Please, introduce a valid method')
         return None
+
+# minimum-variance portfolio given a minimum portfolio return (Markowitz optimization)
+def mean_variance_optimization(data, min_return, initial, expected_returns=None, covariance=None, display=False):
+    """
+    Computes the optimal weights that minimize portfolio variance subject to minimum 
+    level of return in the portfolio using Sequential Least SQuares Programming
+    
+    Parameters
+    ----------
+        data: array_like of shape(M, N) with N > 1
+        min_return: scalar float
+            specifies the minimum return that the portfolio can attain
+        initial: array_like of shape(N, )            
+        expected_returns: array_like of shape (N, )
+        covariance: array_like of shape (N, N)
+        display: boolean, default value is False
+            if it is True, a summary of the optimization procedure is shown
+
+    Return
+    ------
+        out: OptimizeResult object that stores the optimal value and weights
+            among other features after optimization
+            (for more information see scipy.optimize documentation)
+    
+    """
+    if expected_returns == None:
+        expected_returns = np.mean(data, axis=0)
+
+    if covariance == None:
+        covariance = np.covariance(data.T)
+    
+    def func(weights):
+        """
+        Objective function value to optimize
+        """
+        return weights@covariance@weights
+
+    # constraints
+    def const_1(weights):
+        """
+        Contraint 1: weights add up 1
+        """
+        return np.sum(weights) - 1
+
+    def const_2(weights):
+        """
+        Contraint 2: weights are non-negative
+        """     
+        return weights
+
+    def const_3(weights):
+        """
+        Contraint 3: weights are never greater than 1
+        """
+        return 1 - weights
+    
+    def const_4(weights):
+        """
+        Contraint 4: portfolio return can be lower than a given fixed minimum return
+        """
+        return weights@expected_returns - min_return
+    
+    # setting up constraints as dictionary
+    cons = ({'type': 'eq',
+            'fun': lambda x: const_1(x)},
+           {'type': 'ineq',
+           'fun': lambda x: const_2(x)},
+           {'type': 'ineq',
+           'fun': lambda x: const_3(x)},
+           {'type': 'ineq',
+           'fun': lambda x: const_4(x)})
+    
+    # Sequential Least SQuares Programming (SLSQP)
+    res = minimize(func, initial, constraints=cons, method='SLSQP', options={'disp': True})
+    return res
+
+
+def mean_cvar_optimization(data, min_return, beta=0.95, expected_returns=None, display=False):
+    """
+    Computes the optimal weights that minimize beta-CVaR subject to minimum 
+    level of return in the portfolio using linear programming (simplex method)
+    
+    Parameters
+    ----------
+        data: array_like of shape (M, N) where M denotes the number of 
+            draws and N the number of assets each row is a draw from 
+            joint distribution of returns
+        min_return: scalar float
+            specifies the minimum return that the portfolio can attain
+        beta: scalar between 0 and 1, common values for beta are 0.9, 0.95 or 0.99
+        expected_returns: array_like of shape (N, )
+
+    Return
+    ------
+        out: OptimizeResult object that stores the optimal value and weights
+            among other features after optimization
+            (for more information see scipy.optimize documentation)
+    
+    References
+    ----------
+        Rockafellar and Uryasev, Optimization of Conditional Value-At-Risk. The 
+        Journal of Risk, Vol. 2, No. 3, 2000, 21-41 
+    
+    """
+
+    if expected_returns == None:
+        expected_returns = np.mean(data, axis=0)
+
+    # for this version I keep Rockafellar and Uryasev's notation
+    n = data.shape[1]
+    q = data.shape[0]
+    y = data
+    m = expected_returns
+    R = min_return
+    
+    # set minimization problem
+    
+    # define vector for the objective function
+    c = np.hstack([np.zeros(n), np.ones(q)/((1 - beta)*q), 1])
+    
+    # define matrices for equality constraints
+    A_eq = np.hstack([np.ones(n), np.zeros(q + 1)]).reshape(1, n + q + 1)
+    b_eq = np.ones(1)
+    
+    # define matrices for inequality constraints
+    A_ub = np.hstack([-m, np.zeros(q + 1)])
+    A_ub = np.vstack([A_ub, np.hstack([-y, -np.eye(q), -np.ones(q).reshape(q, 1)])])
+    b_ub = np.hstack([-R, np.zeros(q)])
+    
+    # define a sequence for bounds
+    bounds = tuple([(0, None) for _ in range(n + q)] + [(None, None)])
+    
+    # minimize beta-CVaR
+    res = linprog(c, A_ub, b_ub, A_eq, b_eq, bounds)
+    return res
