bb47fcfee6e72657d0aacbadb4f62458a9eef56d
Jesus Ramirez <ramirez_eco@hotmail.com>
2017-11-23 22:45:14 -0600

    modified test.py and test_data.xlsx

--- test.py
+++ test.py
@@ -7,40 +7,71 @@ Created on Thu Nov  9 08:39:33 2017
 
 import pandas as pd
 import numpy as np
-import cvar_optimization as opt
+from scipy.stats import skew, kurtosis
 import matplotlib.pyplot as pp
+from portfolio_optimization import mean_cvar_optimization, efficient_frontier_cvar
+from print_context import print_context
 
-# import real data
-data = pd.read_excel('test_data.xlsx',sheetname='data_sample')
-data = data.iloc[:,[1,2]]/100
-data = data.values
+# import data
+raw_data = pd.read_excel('test_data.xlsx',sheetname='data_sample')
+raw_data = raw_data.iloc[:,1:9]
+raw_data = raw_data.values
 
 # boostraping data
 np.random.seed(0)
+num_obs = len(raw_data)
+indexes = np.random.choice(num_obs,1000)
+data = np.array([raw_data[index] for index in indexes])
 
-# expected return
+# print its mean, variance-covariance, skewness and kurtosis
+with print_context(formatter={'float': '{: 1.4f}'.format}):
+    X = data
+    print('expected returns:')
+    print(np.mean(X, axis=0))
+    print('covariance:')
+    print(np.cov(X.T))
+    print('skewness:')
+    print(skew(X))
+    print('(excess) kurtosis:')
+    print(kurtosis(X))
+
+# optimize a portfolio using mean-cvar framework
+min_return = 0.04
+beta = 0.95
+res = mean_cvar_optimization(data, min_return=min_return, beta=beta)
+
+# print optimal weights
+with print_context(formatter={'float': '{: 1.4f}'.format}):
+    print('Optimal weights:')
+    print(res.x)
+
+# expected return and covariance matrix
 expected_return = np.mean(data,axis=0)
 covariance = np.cov(data.T)
 
-# optimize a portfolio using mean-cvar framework
-min_return = 0.015
-res = opt.mean_cvar_optimization(data, min_return=min_return, beta=0.95)
-
-print('Optimal weights: ({0:1.4f}, {1:1.4f})'.format(res.x[0], res.x[1]))
 print('Optimal Return: {:1.4f}'.format(res.x@expected_return))
 print('Optimal Risk (CVaR): {:1.4f}'.format(res.fun))
 print('Risk (Std. Dev.): {:1.4f}'.format(np.sqrt(res.x@covariance@res.x)))
 
 # compute the efficient frontier and plot it
-returns, risks , cvars, opt_weights = opt.efficient_frontier_cvar(data, beta=0.90)
+num_points = 40
+beta = 0.95
+returns, risks , cvars, weights = efficient_frontier_cvar(data, beta=beta, 
+                                                              num_points=num_points)
 pp.plot(risks, returns)
 pp.title('Efficient Frontier')
 pp.xlabel('Risk')
 pp.ylabel('Return')
 
-df = np.hstack([opt_weights, returns.reshape(10,1), cvars.reshape(10,1), risks.reshape(10,1)])
-df = pd.DataFrame(df,columns=['Weight 1','Weight 2', 'Return', 'Risk (CVaR)', 'SD'])
-pd.options.display.float_format = '{:,.8f}'.format
+# collecting data to write it in Excel
+results = np.hstack([weights, returns.reshape(num_points, 1), 
+                risks.reshape(num_points, 1), cvars.reshape(num_points, 1)])
+
+columns = ['Asset ' + str(i + 1) for i in range(len(expected_return))]
+columns = columns + ['Return', 'Std. Dev.', 'CVaR']
+df = pd.DataFrame(results, columns=columns)
+pd.options.display.float_format = '{:,.4f}'.format
 print(df)
+
 writer = pd.ExcelWriter('results.xlsx')
 df.to_excel(writer,sheet_name='mean_cvar')
